75045037d83dee48e736c0b28941184be7b2769f
Rishabh Mittal <mittalrishabh206@gmail.com>
2022-04-23 18:12:33 +0530

    Initial Commit

---
+++ Assets/ShotManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Shot
+{
+    public float upForce;
+    public float hitForce;
+}
+
+public class ShotManager : MonoBehaviour
+{
+    public Shot topSpin;
+    public Shot flat;
+}
